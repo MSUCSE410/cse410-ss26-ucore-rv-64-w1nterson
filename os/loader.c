cf7d92cc9b6ed30dc5eeb55b82964d9ec99e32f5
David Wesley Krile <115849504+w1nterson@users.noreply.github.com>
2026-03-24 23:43:11 +0000

    neat

--- os/loader.c
+++ os/loader.c
@@ -18,9 +18,11 @@ int finished()
 // Get user progs' infomation through pre-defined symbol in `link_app.S`
 void loader_init()
 {
+	/*
 	if ((uint64)ekernel >= BASE_ADDRESS) {
 		panic("kernel too large...\n");
 	}
+	*/
 	app_info_ptr = (uint64 *)_app_num;
 	app_num = *app_info_ptr;
 	app_info_ptr++;
