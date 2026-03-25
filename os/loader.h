d75d06e520c9868e753029d6c7c56cc525eff940
David Wesley Krile <115849504+w1nterson@users.noreply.github.com>
2026-03-25 00:46:17 +0000

    e

--- os/loader.h
+++ os/loader.h
@@ -11,8 +11,8 @@ void loader_init();
 int run_all_app();
 pagetable_t bin_loader(uint64 start, uint64 end, struct proc *p);
 
-#define MAX_APP_SIZE (0x20000)
-#define BASE_ADDRESS (0x10000)
+//#define MAX_APP_SIZE (0x20000)
+#define BASE_ADDRESS (0x1000)
 #define USTACK_SIZE  (PAGE_SIZE)
 #define KSTACK_SIZE  (PAGE_SIZE)
 #define TRAP_PAGE_SIZE (PAGE_SIZE)
